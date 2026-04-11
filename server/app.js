cf9efc9cb80fa290d56294fb5c7e09ecddbad057
Prabhudharshan <mittaprabhudharshan@gmail.com>
2026-04-11 21:49:46 +0530

    client front-end

--- server/app.js
+++ server/app.js
@@ -1,5 +1,5 @@
 const dotenv= require("dotenv")
-const Express = require("express")
+const express = require("express")
 const cors = require("cors")
 const userRoute = require("./routes/userRoutes.js")
 const blogRoute = require("./routes/blogRoutes.js")
@@ -7,18 +7,18 @@ const connectDB = require("./config/db")
 dotenv.config()
 console.log('JWT_SECRET:', process.env.JWT_SECRET)
 const path = require("path");
-
-app.use(express.static(path.join(__dirname, "../client/build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(
-    path.resolve(__dirname, "../client/build/index.html")
-  );
-});
-const app = Express()
+const app = express()
 
 app.use(cors())
-app.use(Express.json())
+app.use(express.json())
+
+app.use(express.static(path.join(__dirname, "../client")));
+
+app.all(/.*/, (req, res) => {
+  res.sendFile(
+    path.resolve(__dirname, "../client")
+  );
+});
 app.use("/user", userRoute)
 app.use("/blog", blogRoute)
 
